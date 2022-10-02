79f288d4b04e0787465bd9fe9b36873ac060570c
rgRogerio <rogerio.oliveira@uni9.edu.br>
2022-10-02 18:37:23 -0300

    consumindo dados json de uma API fake

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-function enviarForm(){
-  const campo =  document.getElementById('sugestao');
 
-  alert("Voce inseriu o livro " + campo.value+"! Obrigado pela sugestão")
-  
-}
+(function () {
+  function enviarForm() {
+    const campo = document.getElementById('sugestao');
+
+    alert("Voce inseriu o livro " + campo.value + "! Obrigado pela sugestão")
+
+  }
+
+  function getLivros() {
+    fetch("http://localhost:3000/livros")
+      .then((response) => response.json())
+      .then((data) => {
+
+      const ul = document.getElementsByClassName("games-list")
+
+        ul[0].innerHTML = data.map((item,index) => {
+            return(`
+            <li>
+              <a 
+                target="_blank"
+                href="${data[index].url}">
+                <img src="${data[index].urlImage}"
+                alt="${data[index].name}">
+              </a>
+            </li>           
+            
+            `)   
+        })
+
+      })
+
+  }
+  getLivros()
+
+
+
+
+
+
+
+
+
+})();
